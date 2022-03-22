5273e025ebcb13db491f33ace58bd9980a2b52d6
makui <makui@tal.com>
2022-03-22 18:40:29 +0800

    add

--- dto/sso.go
+++ dto/sso.go
@@ -6,3 +6,18 @@ type SsoResponse struct {
 	Msg  string      `json:"msg,omitempty"`
 	Data interface{} `json:"data,omitempty"`
 }
+
+type SsoVerifyResponse struct {
+	Code int         `json:"code"`
+	Msg  string      `json:"msg,omitempty"`
+	Data *UserInfo `json:"data,omitempty"`
+}
+
+type UserInfo struct {
+	AccountId string `json:"account_id,omitempty"`
+	Account   string `json:"account,omitempty"`
+	Name      string `json:"name,omitempty"`
+	Workcode  string `json:"workcode,omitempty"`
+	Email     string `json:"email,omitempty"`
+	YachId    string `json:"yachid,omitempty"`
+}
